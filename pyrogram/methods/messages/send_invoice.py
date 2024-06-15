ad2c3196f8b4a5f2a1ba60047bf39afae334edbe
DAMIEN <65272894+AmineSoukara@users.noreply.github.com>
2024-06-15 14:03:17 +0100

    Update send_invoice.py

--- pyrogram/methods/messages/send_invoice.py
+++ pyrogram/methods/messages/send_invoice.py
@@ -1,24 +1,29 @@
-#  Pyrofork - Telegram MTProto API Client Library for Python
-#  Copyright (C) 2022-present Mayuri-Chan <https://github.com/Mayuri-Chan>
+#  Pyrogram - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
 #
-#  This file is part of Pyrofork.
+#  This file is part of Pyrogram.
 #
-#  Pyrofork is free software: you can redistribute it and/or modify
+#  Pyrogram is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU Lesser General Public License as published
 #  by the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  Pyrofork is distributed in the hope that it will be useful,
+#  Pyrogram is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU Lesser General Public License for more details.
 #
 #  You should have received a copy of the GNU Lesser General Public License
-#  along with Pyrofork.  If not, see <http://www.gnu.org/licenses/>.
-import pyrogram
+#  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
-from pyrogram import types, raw, utils
-from typing import Union, List
+import logging
+from typing import List, Optional, Union
+
+import pyrogram
+from pyrogram import enums, raw, utils, types
+
+log = logging.getLogger(__name__)
+
 
 class SendInvoice:
     async def send_invoice(
@@ -26,154 +31,207 @@ class SendInvoice:
         chat_id: Union[int, str],
         title: str,
         description: str,
-        currency: str,
         payload: Union[str, bytes],
+        currency: str,
         prices: List["types.LabeledPrice"],
-        provider: str = None,
-        provider_data: str = None,
-        photo_url: str = None,
-        photo_size: int = None,
-        photo_mime_type: str = None,
-        start_parameter: str = None,
-        extended_media: "types.InputMedia" = None,
-        reply_to_message_id: int = None,
-        message_thread_id: int = None,
-        message_effect_id: int = None,
-        protect_content: bool = None,
-        quote_text: str = None,
-        quote_entities: List["types.MessageEntity"] = None,
-    ):
+        message_thread_id: Optional[int] = None,
+        provider_token: Optional[str] = None,
+        max_tip_amount: Optional[int] = None,
+        suggested_tip_amounts: Optional[List[int]] = None,
+        start_parameter: Optional[str] = None,
+        provider_data: Optional[str] = None,
+        photo_url: Optional[str] = None,
+        photo_size: Optional[int] = None,
+        photo_width: Optional[int] = None,
+        photo_height: Optional[int] = None,
+        need_name: Optional[bool] = None,
+        need_phone_number: Optional[bool] = None,
+        need_email: Optional[bool] = None,
+        need_shipping_address: Optional[bool] = None,
+        send_phone_number_to_provider: Optional[bool] = None,
+        send_email_to_provider: Optional[bool] = None,
+        is_flexible: Optional[bool] = None,
+        disable_notification: Optional[bool] = None,
+        protect_content: Optional[bool] = None,
+        message_effect_id: Optional[int] = None,
+        reply_to_message_id: Optional[int] = None,
+        reply_markup: Optional[Union[
+            "types.InlineKeyboardMarkup",
+            "types.ReplyKeyboardMarkup",
+            "types.ReplyKeyboardRemove",
+            "types.ForceReply"
+        ]] = None,
+        caption: str = "",
+        parse_mode: Optional["enums.ParseMode"] = None,
+        caption_entities: Optional[List["types.MessageEntity"]] = None
+    ) -> "types.Message":
         """Use this method to send invoices.
 
         .. include:: /_includes/usable-by/bots.rst
 
         Parameters:
             chat_id (``int`` | ``str``):
-                Unique identifier for the target private chat or username of the target private chat.
+                Unique identifier (int) or username (str) of the target chat.
 
             title (``str``):
-                Product name.
+                Product name, 1-32 characters.
 
             description (``str``):
-                Product description.
+                Product description, 1-255 characters
+
+            payload (``str`` | ``bytes``):
+                Bot-defined invoice payload, 1-128 bytes. This will not be displayed to the user, use for your internal processes.
 
             currency (``str``):
-                Three-letter ISO 4217 currency code.
+                Three-letter ISO 4217 currency code, see `more on currencies <https://core.telegram.org/bots/payments#supported-currencies>`_. Pass ``XTR`` for payments in `Telegram Stars <https://t.me/BotNews/90>`_.
 
             prices (List of :obj:`~pyrogram.types.LabeledPrice`):
-                Price with label.
+                Price breakdown, a JSON-serialized list of components (e.g. product price, tax, discount, delivery cost, delivery tax, bonus, etc.). Must contain exactly one item for payments in `Telegram Stars <https://t.me/BotNews/90>`_.
 
-            provider (``str``, *optional*):
-                Payment provider.
-
-            provider_data (``str``, *optional*):
-                Provider data in json format.
-
-            photo_url (``str``, *optional*):
-                Photo URL.
-
-            photo_size (``int``, *optional*):
-                Photo size.
-
-            photo_mime_type (``str``, *optional*):
-                Photo MIME type.
-
-            start_parameter (``str``, *optional*):
-                Unique bot deep-linking parameter that can be used to generate this invoice.
-
-            extended_media (:obj:`~pyrogram.types.InputMedia`, *optional*):
-                Additional media.
+            message_thread_id (``int``, *optional*):
+                If the message is in a thread, ID of the original message.
 
             reply_to_message_id (``int``, *optional*):
                 If the message is a reply, ID of the original message.
 
-            message_thread_id (``int``, *optional*):
-                Unique identifier for the target message thread (topic) of the forum.
-                for forum supergroups only.
+            provider_token (``str``, *optional*):
+                Payment provider token, obtained via `@BotFather <https://t.me/botfather>`_. Pass an empty string for payments in `Telegram Stars <https://t.me/BotNews/90>`_.
+
+            max_tip_amount (``int``, *optional*):
+                The maximum accepted amount for tips in the smallest units of the currency (integer, **not** float/double). For example, for a maximum tip of ``US$ 1.45`` pass ``max_tip_amount = 145``. See the exp parameter in `currencies.json <https://core.telegram.org/bots/payments/currencies.json>`_, it shows the number of digits past the decimal point for each currency (2 for the majority of currencies). Defaults to 0. Not supported for payments in `Telegram Stars <https://t.me/BotNews/90>`_.
+
+            suggested_tip_amounts (List of ``int``, *optional*):
+                An array of suggested amounts of tips in the smallest units of the currency (integer, **not** float/double). At most 4 suggested tip amounts can be specified. The suggested tip amounts must be positive, passed in a strictly increased order and must not exceed ``max_tip_amount``.
+
+            start_parameter (``str``, *optional*):
+                Unique deep-linking parameter. If left empty, **forwarded copies** of the sent message will have a Pay button, allowing multiple users to pay directly from the forwarded message, using the same invoice. If non-empty, forwarded copies of the sent message will have a URL button with a deep link to the bot (instead of a Pay button), with the value used as the start parameter.
+
+            provider_data (``str``, *optional*):
+                JSON-serialized data about the invoice, which will be shared with the payment provider. A detailed description of required fields should be provided by the payment provider.
+
+            photo_url (``str``, *optional*):
+                URL of the product photo for the invoice. Can be a photo of the goods or a marketing image for a service. People like it better when they see what they are paying for.
+
+            photo_size (``int``, *optional*):
+                Photo size in bytes
+
+            photo_width (``int``, *optional*):
+                Photo width
+
+            photo_height (``int``, *optional*):
+                Photo height
+
+            need_name (``bool``, *optional*):
+                Pass True if you require the user's full name to complete the order. Ignored for payments in `Telegram Stars <https://t.me/BotNews/90>`_.
+
+            need_phone_number (``bool``, *optional*):
+                Pass True if you require the user's phone number to complete the order. Ignored for payments in `Telegram Stars <https://t.me/BotNews/90>`_.
+
+            need_email (``bool``, *optional*):
+                Pass True if you require the user's email address to complete the order. Ignored for payments in `Telegram Stars <https://t.me/BotNews/90>`_.
+
+            need_shipping_address (``bool``, *optional*):
+                Pass True if you require the user's shipping address to complete the order. Ignored for payments in `Telegram Stars <https://t.me/BotNews/90>`_.
+
+            send_phone_number_to_provider (``bool``, *optional*):
+                Pass True if the user's phone number should be sent to the provider. Ignored for payments in `Telegram Stars <https://t.me/BotNews/90>`_.
+
+            send_email_to_provider (``bool``, *optional*):
+                Pass True if the user's email address should be sent to the provider. Ignored for payments in `Telegram Stars <https://t.me/BotNews/90>`_.
+
+            is_flexible (``bool``, *optional*):
+                Pass True if the final price depends on the shipping method. Ignored for payments in `Telegram Stars <https://t.me/BotNews/90>`_.
+
+            disable_notification (``bool``, *optional*):
+                Sends the message silently.
+                Users will receive a notification with no sound.
+
+            protect_content (``bool``, *optional*):
+                Protects the contents of the sent message from forwarding and saving.
 
             message_effect_id (``int`` ``64-bit``, *optional*):
                 Unique identifier of the message effect to be added to the message; for private chats only.
-                
-            protect_content (``bool``, *optional*):
-                Protects the contents of the sent message from forwarding and saving.
-                
-            payload (``str`` | ``bytes``):
-                Bot-defined invoice payload, 1-128 bytes. This will not be displayed to the user, use for your internal processes.
 
-            quote_text (``str``, *optional*):
-                Text to quote.
-                for reply_to_message only.
+            reply_markup (:obj:`~pyrogram.types.InlineKeyboardMarkup` | :obj:`~pyrogram.types.ReplyKeyboardMarkup` | :obj:`~pyrogram.types.ReplyKeyboardRemove` | :obj:`~pyrogram.types.ForceReply`, *optional*):
+                Additional interface options. An object for an inline keyboard, custom reply keyboard,
+                instructions to remove reply keyboard or to force a reply from the user.
 
-            quote_entities (List of :obj:`~pyrogram.types.MessageEntity`, *optional*):
-                List of special entities that appear in quote_text, which can be specified instead of *parse_mode*.
-                for reply_to_message only.
+            caption (``str``, *optional*):
+                Document caption, 0-1024 characters.
+
+            parse_mode (:obj:`~pyrogram.enums.ParseMode`, *optional*):
+                By default, texts are parsed using both Markdown and HTML styles.
+                You can combine both syntaxes together.
+
+            caption_entities (List of :obj:`~pyrogram.types.MessageEntity`):
+                List of special entities that appear in the caption, which can be specified instead of *parse_mode*.
 
         Returns:
-            :obj:`~pyrogram.types.Message`: On success, the sent message is returned.
+            :obj:`~pyrogram.types.Message`: On success, the sent invoice message is returned.
 
-        Example:
-            .. code-block:: python
-
-                app.send_invoice(chat_id, types.InputMediaInvoice(
-                    title="Product Name",
-                    description="Product Description",
-                    currency="USD",
-                    prices=[types.LabeledPrice("Product", 1000)],
-                    provider="Stripe",
-                    provider_data="{}"
-                ))
         """
-
-        reply_to = await utils.get_reply_to(
-            client=self,
-            chat_id=chat_id,
-            reply_to_message_id=reply_to_message_id,
-            message_thread_id=message_thread_id,
-            quote_text=quote_text,
-            quote_entities=quote_entities
+        media = raw.types.InputMediaInvoice(
+            title=title,
+            description=description,
+            photo=raw.types.InputWebDocument(
+                url=photo_url,
+                mime_type="image/jpg",
+                size=photo_size,
+                attributes=[
+                    raw.types.DocumentAttributeImageSize(
+                        w=photo_width,
+                        h=photo_height
+                    )
+                ]
+            ) if photo_url else None,
+            invoice=raw.types.Invoice(
+                currency=currency,
+                prices=[i.write() for i in prices],
+                test=self.test_mode,
+                name_requested=need_name,
+                phone_requested=need_phone_number,
+                email_requested=need_email,
+                shipping_address_requested=need_shipping_address,
+                flexible=is_flexible,
+                phone_to_provider=send_phone_number_to_provider,
+                email_to_provider=send_email_to_provider,
+                max_tip_amount=max_tip_amount,
+                suggested_tip_amounts=suggested_tip_amounts
+            ),
+            payload=payload.encode() if isinstance(payload, str) else payload,
+            provider=provider_token,
+            provider_data=raw.types.DataJSON(
+                data=provider_data if provider_data else "{}"
+            ),
+            start_param=start_parameter
         )
 
-        r = await self.invoke(
-            raw.functions.messages.SendMedia(
-                peer=await self.resolve_peer(chat_id),
-                media=raw.types.InputMediaInvoice(
-                    title=title,
-                    description=description,
-                    invoice=raw.types.Invoice(
-                        currency=currency,
-                        prices=[price.write() for price in prices]
-                    ),
-                    payload=payload.encode() if isinstance(payload, str) else payload,
-                    provider=provider,
-                    provider_data=raw.types.DataJSON(data=provider_data if provider_data else "{}"),
-                    photo=raw.types.InputWebDocument(
-                        url=photo_url,
-                        size=photo_size or 0,
-                        mime_type=photo_mime_type or "image/jpeg",
-                        attributes=[]
-                    ) if photo_url else None,
-                    start_param=start_parameter,
-                    extended_media=extended_media
-                ),
-                random_id=self.rnd_id(),
-                reply_to=reply_to,
-                effect=message_effect_id,
-                noforwards=protect_content,
-                message=""
-            )
+        rpc = raw.functions.messages.SendMedia(
+            peer=await self.resolve_peer(chat_id),
+            media=media,
+            silent=disable_notification or None,
+            reply_to=utils.get_reply_to(
+                reply_to_message_id=reply_to_message_id,
+                message_thread_id=message_thread_id
+            ),
+            random_id=self.rnd_id(),
+            noforwards=protect_content,
+            reply_markup=await reply_markup.write(self) if reply_markup else None,
+            effect=message_effect_id,
+            **await utils.parse_text_entities(self, caption, parse_mode, caption_entities)
         )
+
+        r = await self.invoke(rpc)
 
         for i in r.updates:
-            if isinstance(
-                i,
-                (
-                    raw.types.UpdateNewMessage,
-                    raw.types.UpdateNewChannelMessage
-                )
-            ):
+            if isinstance(i, (raw.types.UpdateNewMessage,
+                              raw.types.UpdateNewChannelMessage,
+                              raw.types.UpdateNewScheduledMessage,
+                              raw.types.UpdateBotNewBusinessMessage)):
                 return await types.Message._parse(
-                    self,
-                    i.message,
-                    users={i.id: i for i in r.users},
-                    chats={i.id: i for i in r.chats}
+                    self, i.message,
+                    {i.id: i for i in r.users},
+                    {i.id: i for i in r.chats},
+                    is_scheduled=isinstance(i, raw.types.UpdateNewScheduledMessage),
+                    business_connection_id=getattr(i, "connection_id", None)
                 )
